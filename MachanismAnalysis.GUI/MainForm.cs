d686030b551f1b45fd0125d20b1d13c57c83b089
Cheng Ziqiu <jxlpzqc@live.com>
2020-07-04 08:47:35 +0800

    Add project files.

---
+++ MachanismAnalysis.GUI/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachanismAnalysis.GUI
+{
+    class MainForm:Form
+    {
+        private const int initHeight = 600;
+        private const int initWidth = 800;
+        
+
+        public MainForm()
+        {
+            Text = "机械原理课程设计 - 图形用户界面 - 程子丘";
+            Height = initHeight;
+            Width = initWidth;
+            BackColor = Color.White;
+
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.UserPaint,true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+
+            UpdateStyles();
+
+            Graphics = CreateGraphics();
+
+            Timer.Tick += (obj,e) =>
+            {
+                Draw();
+
+            };
+            Timer.Start();
+        }
+
+        public Timer Timer { get; set; } = new Timer()
+        {
+            Interval = 13            
+        };
+
+        private int realHeight = 100;
+        private int realWidth = 100;
+
+        private Point CreatePoint(double realX, double realY)
+        {
+            //TODO 修复缩放问题
+
+            return new Point()
+            {
+                X = (int) ConvertToScreenX(realX),
+                Y = (int) ConvertToScreenY(realY)
+            };
+        }
+
+        private double ConvertToScreenX(double realX)
+        {
+            return (realX / realWidth * Width);
+        }
+        private double ConvertToScreenY(double realY)
+        {
+            
+            return (realY / realHeight * Height);
+        }
+
+
+        private static class Pens
+        {
+            public static Pen MechanismPen = new Pen(Color.Black);
+
+        }
+
+        /// <summary>
+        /// 根据图像指定画布大小
+        /// </summary>
+        private void DetermineCanvasSize()
+        {
+            // TODO 完成他
+
+        }
+
+
+        private Graphics Graphics { get; }
+
+
+
+
+        private void DrawFrame(double x,double y)
+        {
+            Graphics.DrawArc(Pens.MechanismPen, new Rectangle(CreatePoint(x - 10, y - 10), new Size(10, 10)), 0, 360);
+
+
+
+        }
+
+        public void Draw()
+        {
+            Graphics.Clear(Color.White);
+
+            Graphics.DrawLine(new Pen(Color.Red) { 
+                Width = 20
+            }, CreatePoint(10, 20), CreatePoint(20, 30));
+
+
+        }
+
+
+    }
+}
---
+++ MachanismAnalysis.GUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachanismAnalysis.GUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            AllocConsole();
+            SetConsoleTitle("机械原理课程设计 - 控制台输出");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+
+            FreeConsole();
+        }
+
+
+        #region 本地Win32代码导入
+
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeConsole();
+        [DllImport("kernel32.dll")]
+        public static extern bool SetConsoleTitle(string title);
+
+        #endregion
+
+    }
+}
